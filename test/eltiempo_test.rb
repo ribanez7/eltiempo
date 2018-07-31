25b5b97e9b18c370a02dcaaab7156cff9f7d0988
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2018-07-31 12:35:27 +0200

    Adding eltiempo.rb test

--- test/eltiempo_test.rb
+++ test/eltiempo_test.rb
@@ -4,4 +4,11 @@ class EltiempoTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Eltiempo::VERSION
   end
+
+  def test_it_delegates_the_logic_to_the_reporter_main_class
+    pattern = /Results for Terrassa, today:\n\d{1,2}° \/ \d{1,2}°\n/
+    assert_output(pattern) do
+      Eltiempo.r(municipality: 'Terrassa')
+    end
+  end
 end
