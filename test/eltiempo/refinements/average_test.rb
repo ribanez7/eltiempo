7cb876097f38f97acafa5088c16d24e82a2d5b3e
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2018-07-31 08:48:30 +0200

    refinements tests

---
+++ test/eltiempo/refinements/average_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+require 'eltiempo/refinements/average'
+
+module Eltiempo
+  module Refinements
+    class AverageTest < Minitest::Test
+      class Dummy
+        using ::Eltiempo::Refinements::Average
+
+        def integers_average
+          [1, 2, 3, 4, 5].average
+        end
+
+        def floats_average
+          [5.1, 4.6, 9.8, 20.7, 8.0].average
+        end
+      end
+
+      def setup
+        @mock = Dummy.new
+      end
+
+      def test_an_array_knows_how_to_calculate_integers_average
+        assert_equal @mock.integers_average, 3
+      end
+
+      def test_an_array_knows_how_to_calculate_floats_average
+        assert_equal @mock.floats_average, 9.64
+      end
+    end
+  end
+end
---
+++ test/eltiempo/refinements/query_param_find_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+require 'eltiempo/refinements/query_param_find'
+
+module Eltiempo
+  module Refinements
+    class QueryParamFindTest < Minitest::Test
+      class Dummy
+        using ::Eltiempo::Refinements::QueryParamFind
+
+        def url_from_google
+          <<~EOS.delete("\n")
+            https://www.google.com/search?source=hp&ei=3gRgW_THO4GclwTN57GQDQ
+            &q=buenos+d%C3%ADas&oq=buenos+d%C3%ADas&gs_l=psy-ab.3..0i131k1l3j
+            0l7.4227.5840.0.5986.13.9.0.3.3.0.110.678.7j1.9.0....0...1c.1j4.6
+            4.psy-ab..1.12.792.6..35i39k1j0i67k1.90.RGghi9OPeL0
+          EOS
+        end
+
+        def get_the_search_term
+          url_from_google.query_param_find('q')
+        end
+      end
+
+      def setup
+        @mock = Dummy.new
+      end
+
+      def test_a_string_knows_how_to_get_a_query_param
+        assert_equal @mock.get_the_search_term, 'buenos días'
+      end
+    end
+  end
+end
